6526420b03136a1cf5edb4a7767d1f3d0b4af096
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-29 08:46:35 +0600

    added solution of first problem

---
+++ task-1.js
@@ -0,0 +1,8 @@
+// convert temperature celsious to fahrenheit
+// °F = (9/5 × °C) + 32.
+
+const temperature = 200;
+const fahrenheit = (9 / 5 * temperature) + 32;
+// console.log('F =',fahrenheit);
+
+//
